0cb74f0160de5c541ddd609a292ba04326deaa3e
Mackelprang <rmackelpr@rachel1-eh2207.local>
2021-11-30 10:58:38 -0800

    permancova tests

--- Permancova.R
+++ Permancova.R
@@ -7,13 +7,14 @@ library(factoextra)
 library(ggfortify)
 library(phyloseq)
 library(vegan)
+library(BiodiversityR)
 
 
 set.seed(42)
 
 
 ###################################
-######import and transform metadata
+######Import and transform metadata
 ###################################
 
 meta<-read_excel("metadata.xlsx")
@@ -41,7 +42,7 @@ meta$electrical_cond<-log10(meta$electrical_cond+1)
 meta$pH<-meta$pH^(3)
 
 ################################
-#import and manipulate KEGG data
+#Import and manipulate KEGG data
 ################################
 
 #import KEGG data (counts)
@@ -63,17 +64,19 @@ kegg_trimmed_ra<-as.data.frame(otu_table(physeq.f.ra))
 kegg_trimmed_ra<-t(kegg_trimmed_ra)
 
 ###############################
-#imputations###################
+#Imputations###################
 ###############################
 
 #impute missing values of continuous variables (except longitude)
 imputed<-amelia(meta, m=1, idvars=c("Site", "Region1", "Type", "Origin", "Region2", "Period1", "Longitude", "Period2"))
 imputed_df<-as.data.frame(imputed$imputations)
 
-#normalize by subtracting the mean and dividing by the standard deviation. This also selects only one
+#normalize by subtracting the mean and dividing by the standard deviation. Here, we are also selecting only one
 #of the imputations output by amelia. Note we are excluding longitude
 
 imputed_norm_cont<-scale(imputed_df[c(8,10:19)])
+
+
 
 ##############################
 #Correlation matrix and PCA###
@@ -99,41 +102,59 @@ meta_norm_zscores<-as.data.frame(meta_norm_zscores)
 
 
 #############################
-#data wrangling. 
+#Data wrangling. 
 #############################
 
-#add site category and region1 to PC dataframe
+#add site category and region1 to dataframes
 meta_norm_zscores<-cbind(meta_norm_zscores, meta$Site)
 meta_norm_zscores<-cbind(meta_norm_zscores, meta$Region1)
+
+imputed_norm_cont_df<-as.data.frame(cbind(imputed_norm_cont, meta$Site))
+imputed_norm_cont_df<-cbind(imputed_norm_cont, meta$Region1)
+
 
 #change column name of Site from "meta$Site" to Site
 colnames(meta_norm_zscores)[colnames(meta_norm_zscores) == "meta$Site"] <- "Site"
 colnames(meta_norm_zscores)[colnames(meta_norm_zscores) == "meta$Region1"] <- "Region1"
 
+
+colnames(imputed_norm_cont_df)[colnames(imputed_norm_cont_df) == "meta$Site"] <- "Site"
+colnames(imputed_norm_cont_df)[colnames(imputed_norm_cont_df) == "meta$Region1"] <- "Region1"
+
 #Find only samples that overlap between KEGG data and metadata
-kegg_meta_common_ids<-intersect(rownames(meta_norm_zscores), rownames(kegg_trimmed_ra))
-kegg_trimmed_ra_common<-kegg_trimmed_ra[kegg_meta_common_ids,]
-meta_norm_zscores_common<-meta_norm_zscores[kegg_meta_common_ids,]
-
+#kegg_meta_common_ids<-intersect(rownames(meta_norm_zscores), rownames(kegg_trimmed_ra))
+#kegg_trimmed_ra_common<-kegg_trimmed_ra[kegg_meta_common_ids,]
+#meta_norm_zscores_common<-meta_norm_zscores[kegg_meta_common_ids,]
 
 
 ##############################
-#PERMANCOVA###################
+#PERMANCOVA & PERMANOVA#######
 ##############################
 
 
-#square root transform kegg relative abundance data
-kegg_sqrt<-sqrt(kegg_trimmed_ra_common[,1:5504])
+
+#square root transform kegg relative abundance data. 5504 is the number of genes left after removing low abundance genes
+kegg_sqrt<-sqrt(kegg_trimmed_ra[,1:5504])
 
 #get bray-curtis dissimilarities
 kegg_bray<-vegdist(kegg_sqrt, "bray")
 
-
-
 #permanova
+kegg_adonis<-adonis(formula=kegg_bray~Site*Region1*imp1.Latitude*imp1.Elevation_m*imp1.KYBP*
+                      #imp1.Depth_m* imp1.TotalC_percent*imp1.TotalN_percent *imp1.CN* 
+                      #mp1.OrganicC_percent* imp1.pH*imp1.water.content.gwat.per.gsoil*
+                      imp1.electrical_cond, data=imputed_norm_cont)
+
+
+#permancova
 
 #Comp.1 and Comp.2 are the z-scores from the PCA
-kegg_PC1_PC2_adonis<-adonis(formula=kegg_bray~Site*Comp.1+Site*Comp.2, data=meta_norm_zscores_common, permutations=1000)
-
+kegg_PC1_PC2_adonis<-adonis(formula=kegg_bray~Comp.1*Comp.2, data=meta_norm_zscores, 
+                            strata=meta_norm_zscores$Site+meta_norm_zscores$Region1, permutations=1000)
 kegg_PC1_PC2_adonis
+
+
+kegg_adonis<-adonis(formula=kegg_bray~Site, data=meta, permutations=1000)
+kegg_adonis
+
 
